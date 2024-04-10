455a54ffe7380f8cc74506ba69bcac79da612dc9
Rohit Rao <rohitk121999@gmail.com>
2024-04-10 12:48:59 +0530

    updated chat history

--- locallama.py
+++ locallama.py
@@ -1,3 +1,4 @@
+import pickle
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -23,12 +24,29 @@ prompt=ChatPromptTemplate.from_messages(
 ## streamlit framework
 
 st.title('Langchain API Demo With LLAMA2 ')
+
+# Load chat history from file (if it exists)
+try:
+    with open('chat_history.pkl', 'rb') as f:
+        chat_history = pickle.load(f)
+except FileNotFoundError:
+    chat_history = []
+
 input_text=st.text_input("Search the topic u want")
 
-# ollama LLAma2 LLm 
+# ollama LLAma2 LLm
 llm=Ollama(model="llama2")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
+    chat_history.append((input_text, chain.invoke({"question":input_text})))
+    st.write("You: ", input_text)
+    st.write("Assistant: ", chat_history[-1][-1])
+    for old_query, old_response in chat_history[:-1]:
+        st.write("You: ", old_query)
+        st.write("Assistant: ", old_response)
+
+# Save chat history to file
+with open('chat_history.pkl', 'wb') as f:
+    pickle.dump(chat_history, f)
